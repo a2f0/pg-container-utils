0c43c286ac5daaa47d616f330d06e7ee4582ddb7
Dan Sullivan <dansullivan@gmail.com>
2025-09-14 13:26:40 -0400

    fix: refresh collation version

--- bin/restore.sh
+++ bin/restore.sh
@@ -6,6 +6,10 @@ echo "=== Performing restore to $POSTGRES_DB"
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 
+echo "=== Refreshing collation versions"
+psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d postgres -c "ALTER DATABASE postgres REFRESH COLLATION VERSION;" 2>/dev/null || true
+psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d postgres -c "ALTER DATABASE template1 REFRESH COLLATION VERSION;" 2>/dev/null || true
+
 echo "=== Dropping $POSTGRES_DB"
 dropdb "$POSTGRES_DB" -h "$POSTGRES_HOST" -U "$POSTGRES_USER" --if-exists
 
